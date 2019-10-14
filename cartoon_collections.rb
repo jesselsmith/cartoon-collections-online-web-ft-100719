8f5c72ad77a4fb2a7dd622bda882e788e5f178e4
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-14 18:01:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarf_array)
-  dwarf_array.each_with_index{|dwarf, index| puts "#{index}. #{dwarf}"
+  dwarf_array.each_with_index{|dwarf, index| puts "#{index}. #{dwarf}"}
 end
 
 def summon_captain_planet# code an argument here
